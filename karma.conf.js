7b4560273f0acb8c7b86eab5c3a44f5cfdeaaa56
Andy Smircich <andy@smokeybayair.com>
2018-08-11 06:18:36 +0000

    angular bootstrap compatible with v4 morgul.github.io/ui-bootstrap4/

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,8 @@ module.exports = function(config) {
       'client/bower_components/angular-socket-io/socket.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
       'client/bower_components/angular-validation-match/dist/angular-validation-match.min.js',
+      'client/bower_components/angular-animate/angular-animate.js',
+      'client/bower_components/angular-touch/angular-touch.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'node_modules/socket.io-client/socket.io.js',
