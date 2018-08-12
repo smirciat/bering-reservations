55bda484ae0771bdebbd10bd3d0956588d9ab225
Andy Smircich <andy@smokeybayair.com>
2018-08-12 05:01:55 +0000

    dropdown form

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -11,8 +11,9 @@
       this.timeout=$timeout;
       this.awesomeThings = [];
       this.data={name:{'2,1':'hereisis'},village:{}};
-      this.seven=[1,2,3,4,5,6,7];
-      this.twelve=[1,2,3,4,5,6,7,8,9,10,11,12];
+      this.cols=[1,2,3,4,5,6,7];
+      this.rows=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+      this.isOpen={};
       this.col=1;
       this.row=1;
       this.keys = [];
@@ -60,12 +61,35 @@
     }
     
     updateFocus(){
+      if (this.dropdownOpen()==="true") return;
+      if (this.col<1) this.col=1;
+      if (this.row<1) this.row=1;
+      if (this.col>this.cols.length) this.col=this.cols.length;
+      if (this.row>this.rows.length) this.row=this.rows.length;
       var myId=this.col+","+this.row;
-      console.log(myId);
       this.timeout(()=>{
         var myEl = this.window.document.getElementById(myId);
         if (myEl) myEl.focus();
       });
+    }
+    
+    dropdownOpen(){
+      var open="false";
+      for (var c=0;c<this.cols.length;c++){
+        for (var r=0;r<this.rows.length;r++){
+          if (this.isOpen[this.cols[c]+','+this.rows[r]]){
+            open="true";
+            c=this.cols.length;
+            r=this.rows.length;
+          }
+        }
+      }
+      return open;
+    }
+    
+    bottomFour(row){
+      if (this.rows[this.rows.length-1]-row<7) return "dropup";
+      else return;
     }
   }
 
